66bc174fb5f99f100cc09f1b07e73f5f75f77d2c
Suyash <suyashrajsingh424@gmail.com>
2025-12-24 19:15:48 +0530

    Designed Video Upload Form

--- app/(app)/video-upload/page.tsx
+++ app/(app)/video-upload/page.tsx
@@ -1,4 +1,6 @@
-import React, {useState} from "react";
+"use client"
+
+import React, { useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 
@@ -13,16 +15,72 @@ function VideoUpload() {
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-        if(!file) return;
-        if(file.size > MAX_FILE_SIZE) {
+        if (!file) return;
+        if (file.size > MAX_FILE_SIZE) {
             alert("File size limit exceeded");//notification will be here in future
             return;
+        }
+
+        setIsUploading(true);
+        const formData = new FormData();
+        formData.append("file", file);
+        formData.append("title", title);
+        formData.append("description", description);
+        formData.append("originalSize", file.size.toString());
+
+        try {
+            const response = await axios.post("/api/video-upload", formData);
+            if (response.status >= 200 && response.status < 300) {
+
+            }
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setIsUploading(false);
         }
     }
 
     return (
-        <div>
+        <div className="container mx-auto p-4 max-w-4xl">
+            <h1 className="heading-2 text-center">Video Upload Section</h1>
+            <form className="space-y-4 card" onSubmit={handleSubmit}>
+                <div>
+                    <label>Title</label>
+                    <input
+                        type="text"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        className="input-base"
+                    />
+                </div>
 
+                <div>
+                    <label>Description</label>
+                    <textarea
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    className="input-base"
+                    />
+                </div>
+
+                <div>
+                    <label>Video File</label>
+                    <input 
+                    type="file"
+                    accept="video/*"
+                    onChange={(e) => setFile(e.target.files?.[0] || null)}
+                    className="input-base"
+                    />
+                </div>
+
+                <button 
+                type="submit"
+                className="btn btn-primary"
+                disabled={isUploading}
+                >
+                    {isUploading? "Uploading...": "Upload"}
+                </button>
+            </form>
         </div>
     )
 }
